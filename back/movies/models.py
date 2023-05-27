29b4634a088f26c09b990ea074f0c6cdc7a47c6c
wisenick1 <wisenick1@naver.com>
2023-05-28 01:22:13 +0900

    movie db models.py 생성

--- back/movies/models.py
+++ back/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    item_name = models.TextField()
+    genre1 = models.TextField()
+    genre2 = models.TextField()
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'movies'
