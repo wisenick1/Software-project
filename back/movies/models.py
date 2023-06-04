52b71f040e179e25887659e24497dfb25bdf42d4
wisenick1 <wisenick1@naver.com>
2023-06-04 16:15:03 +0900

    utf-8, user 등등

--- back/movies/models.py
+++ back/movies/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Movie(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     item_name = models.TextField()
     genre1 = models.TextField()
     genre2 = models.TextField()
